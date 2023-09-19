2569f0ddf78d14079ab13feac1a226ba91d506dd
Fabricio Sanchez <132334527+FaaDoge@users.noreply.github.com>
2023-09-19 11:40:37 -0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 // Configuración de la conexión a la base de datos
 const pool = new Pool({
-  connectionString: 'postgres://fabricio:Wu8dBwO6gschIwRyjzQXCWhyvIfgNVPB@dpg-cj09egj438irjjbasg50-a.oregon-postgres.render.com/icpc',
+  connectionString: '',
   ssl: {
     rejectUnauthorized: false
   }
